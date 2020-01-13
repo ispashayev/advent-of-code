704a127b51057f62f545b7ec714b4891d2ceea0d
Iskandar Pashayev <iskandar@captivateiq.com>
2020-01-13 13:09:47 -0800

    day 14 part 1 wip

--- 2018/day14.go
+++ 2018/day14.go
@@ -1,32 +1,45 @@
 package main
 
 import (
-  "fmt"
-  "io/ioutil"
-  "strings"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
 )
 
 var kNumPlayers = 2
 
 func check(err error) {
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
-  dat, err := ioutil.ReadFile("day14.dat")
-  check(err)
+	dat, err := ioutil.ReadFile("day14.dat")
+	check(err)
 
-  scoreboardInput := strings.TrimSpace(string(dat))
-  scoreboard := make([]int, len(scoreboardInput))
-  for i, score := range scoreboardInput {
-    scoreboard[i] = int(score - '0')
-  }
-  
-  fmt.Printf("The input scoreboard is: %s\n", scoreboardInput)
-  fmt.Println("The recipe scores are:")
-  for i, score := range scoreboard {
-    fmt.Printf("Recipe %d: %d\n", i, score)
-  }
+	input := strings.TrimSpace(string(dat))
+	numRecipes, err := strconv.Atoi(input)
+	check(err)
+
+	fmt.Printf("The input number of recipes is: %d\n", numRecipes)
+
+	scoreboard := []int{3, 7}
+	e1pos, e2pos := 0, 1
+
+	for len(scoreboard) < numRecipes+10 {
+		e1score, e2score := scoreboard[e1pos], scoreboard[e2pos]
+		newRecipes := strconv.Itoa(e1score + e2score)
+		for _, newRecipe := range newRecipes {
+			scoreboard = append(scoreboard, int(newRecipe-'0'))
+		}
+		e1pos = (1 + e1score) % len(scoreboard)
+		e2pos = (1 + e2score) % len(scoreboard)
+	}
+
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%d ", scoreboard[numRecipes+i])
+	}
+	fmt.Println()
 }
